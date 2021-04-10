813eb949a0fd2cdcbc7d1c1c62a05d39a6c4e171
Haresh Bhuriya <haresh.bhuriya@thalesgroup.com>
2021-04-10 20:40:11 +0530

    Dependency ingestion using spring

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,36 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controller.ConstructorInjectedController;
+import guru.springframework.sfgdi.controller.MyController;
+import guru.springframework.sfgdi.controller.PropertyInjectedController;
+import guru.springframework.sfgdi.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        MyController myController = (MyController) ctx.getBean("myController");
+        String greetings = myController.sayHello();
+        System.out.println(greetings);
+
+        System.out.println("--- Property ---");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        String greetingProp = propertyInjectedController.getGreeting();
+        System.out.println(greetingProp);
+
+        System.out.println("--- Setter ---");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        String greetingSetter = setterInjectedController.getGreeting();
+        System.out.println(greetingSetter);
+
+        System.out.println("--- Constructor ---");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+        String greetingConst = setterInjectedController.getGreeting();
+        System.out.println(greetingConst);
     }
 
 }
